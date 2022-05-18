601e873b4c2f0d3614c4c201540704ac7f18fcf8
Роман Шапарь <romanshaparution@gmail.com>
2022-05-18 09:11:00 +0300

    modif

--- src/test/java/ru/netology/CardDeliveryIbankTest.java
+++ src/test/java/ru/netology/CardDeliveryIbankTest.java
@@ -13,45 +13,45 @@ import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.DataGenerator.getUserInfo;
 
 public class CardDeliveryIbankTest {
-        DataGenerator.UserInfo user = getUserInfo();
+    DataGenerator.UserInfo user = getUserInfo();
 
-        @BeforeEach
-        public void setUp() {
-                open("http://localhost:9999/");
-        }
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+    }
 
-        @Test
-        public void shouldValidIfChangeDate() {
+    @Test
+    public void shouldValidIfChangeDate() {
 
-                $("[placeholder='Город']").setValue(user.getCity());
-                $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-                $("[placeholder='Дата встречи']").setValue(user.getMeetingDate());
-                $("[name='name']").setValue(user.getFullName());
-                $("[name='phone']").setValue(user.getMobilePhone());
-                $(".checkbox__box").click();
-                $(withText("Запланировать")).click();
-                $("[data-test-id='success-notification'] > .notification__title")
-                        .shouldHave(exactText("Успешно!"))
-                        .shouldBe(Condition.visible);
-                $("[data-test-id='success-notification'] > .notification__content")
-                        .shouldHave(text("Встреча успешно запланирована на " + user.getMeetingDate()))
-                        .shouldBe(Condition.visible);
-                $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-                $("[placeholder='Дата встречи']").setValue(user.getDateRescheduling());
-                $(withText("Запланировать")).click();
-                $("[data-test-id='replan-notification'] > .notification__title")
-                        .shouldHave(exactText("Необходимо подтверждение"))
-                        .shouldBe(Condition.visible);
-                $("[data-test-id='replan-notification'] > .notification__content")
-                        .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                        .shouldBe(Condition.visible);
-                $(withText("Перепланировать")).click();
-                $("[data-test-id='success-notification'] > .notification__title")
-                        .shouldHave(exactText("Успешно!"))
-                        .shouldBe(Condition.visible);
-                $("[data-test-id='success-notification'] > .notification__content")
-                        .shouldHave(exactText("Встреча успешно запланирована на " + user.getDateRescheduling()))
-                        .shouldBe(Condition.visible);
-        }
+        $("[placeholder='Город']").setValue(user.getCity());
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(user.getMeetingDate());
+        $("[name='name']").setValue(user.getFullName());
+        $("[name='phone']").setValue(user.getMobilePhone());
+        $(".checkbox__box").click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='success-notification'] > .notification__title")
+                .shouldHave(exactText("Успешно!"))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] > .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + user.getMeetingDate()))
+                .shouldBe(Condition.visible);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(user.getDateRescheduling());
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] > .notification__title")
+                .shouldHave(exactText("Необходимо подтверждение"))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='replan-notification'] > .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(Condition.visible);
+        $(withText("Перепланировать")).click();
+        $("[data-test-id='success-notification'] > .notification__title")
+                .shouldHave(exactText("Успешно!"))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] > .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + user.getDateRescheduling()))
+                .shouldBe(Condition.visible);
+    }
 
 }
